3715d00e9feed91b584b7b5b9a977b192365c4fc
kaioferreiira <kaioferreiracosta@hotmail.com>
2018-06-07 15:06:50 -0300

    editanto projeto: salvando fotos temporariamente

--- brewer/src/main/java/com/algaworks/brewer/storage/FotoStorage.java
+++ brewer/src/main/java/com/algaworks/brewer/storage/FotoStorage.java
@@ -5,6 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FotoStorage {
 
-	public void salvarTemporariamente(MultipartFile[] files);
+	public String salvarTemporariamente(MultipartFile[] files);
 	
 }
